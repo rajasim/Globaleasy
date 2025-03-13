7c4529085cf676a0b2022d03f4b944c211e1fc36
Rajasim <Rajmahat@13gmail.com>
2025-03-13 12:40:09 +0530

    raj

--- src/javascript_file/Footer.jsx
+++ src/javascript_file/Footer.jsx
@@ -93,7 +93,7 @@ function Footer() {
    
     </div>
     <div className="footerdown">
-      © 2025 <span className="highlight">Global Easy Connect pvt </span> | All Rights Reserved. | Made By  ABM Group
+      © 2024 <span className="highlight">Easy Global Connect pvt </span> | All Rights Reserved. | Made By  ABM Group
     </div>
     </>
   );
